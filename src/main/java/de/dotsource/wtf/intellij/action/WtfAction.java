59de3e2f22c79234d0811c8a898bb99fde199154
mmi <m.maibohm@dotsource.de>
2019-10-25 22:16:33 +0200

    AnActionEvent returns SelectionStart, SelectionEnd and Filepath

--- src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
+++ src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
@@ -1,7 +1,14 @@
 package de.dotsource.wtf.intellij.action;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.FilePath;
+import com.intellij.openapi.vcs.actions.VcsContext;
+import com.intellij.openapi.vcs.actions.VcsContextFactory;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +23,15 @@ public class WtfAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent actionEvent) {
-        LOG.error("Action Performed with {}", actionEvent);
+        Caret caret = actionEvent.getData(PlatformDataKeys.CARET);
+        int lineStart = caret.getSelectionStartPosition().line;
+        int lineEnd = caret.getSelectionEndPosition().line;
+        VirtualFile file = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE);
+        //VcsContextFactory vcsContextFactory = PeerFactory.getInstance().getVcsContextFactory()
+        //  VcsContext context = vcsContextFactory.createContextOn(actionEvent);
+        String path = file.getCanonicalPath();
+
+        LOG.error("Selection starts in Line {} and ends in Line {} in file {}", lineStart, lineEnd, path);
     }
 
     @Override
