9e93211574cb676dee0b36ec6193fa372db9795c
Johannes Ballmann <fibsifan+github@gmail.com>
2019-10-25 20:59:38 +0200

    WtfAction: Start by logging Line-Numbers

--- src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
+++ src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
@@ -2,6 +2,10 @@ package de.dotsource.wtf.intellij.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +21,12 @@ public class WtfAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent actionEvent) {
         LOG.error("Action Performed with {}", actionEvent);
+        final Project project = actionEvent.getData(CommonDataKeys.PROJECT);
+        final Editor editor = actionEvent.getData(CommonDataKeys.EDITOR);
+        final Caret caret = actionEvent.getData(CommonDataKeys.CARET);
+        int lineNumber = caret.getLogicalPosition().line;
+        caret.getSelectionStart();
+        LOG.error("Caret is at line {}, Selection starts at {}.", lineNumber, null);
     }
 
     @Override
