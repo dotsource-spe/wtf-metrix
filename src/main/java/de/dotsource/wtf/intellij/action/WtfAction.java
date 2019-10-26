83356ca6368299a0e02ec990ebaa37d3ab9b7009
Johannes Ballmann <fibsifan+github@gmail.com>
2019-10-26 06:43:20 +0200

    Make WTF submissions work.

--- src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
+++ src/main/java/de/dotsource/wtf/intellij/action/WtfAction.java
@@ -2,8 +2,10 @@ package de.dotsource.wtf.intellij.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.RunResult;
+import git4idea.repo.GitRepositoryManager;
+import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +17,21 @@ import org.slf4j.LoggerFactory;
  */
 public class WtfAction extends AnAction {
     private static final Logger LOG = LoggerFactory.getLogger(WtfAction.class);
-    private static final ReadAction<AnActionEvent> READ_GIT_RESULT_ACTION = new ReadGitResultAction();
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent actionEvent) {
-        RunResult<AnActionEvent> runResult = new RunResult<>(READ_GIT_RESULT_ACTION);
-        runResult.run();
+        RunResult<AnActionEvent> runResult = new RunResult<>(new ReadGitResultAction(actionEvent));
+        ApplicationManager.getApplication().executeOnPooledThread(runResult::run);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent actionEvent) {
+        actionEvent.getPresentation().setEnabled(isPossible(actionEvent));
+    }
+
+    private boolean isPossible(@NotNull AnActionEvent actionEvent) {
+        return actionEvent.getProject() != null &&
+                CollectionUtils.isNotEmpty(GitRepositoryManager.getInstance(actionEvent.getProject()).getRepositories());
     }
 
     @Override
