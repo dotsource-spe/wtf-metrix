5ad38a98ebaf5b94c515f7cf8a2c0246a0c1e78b
Johannes Ballmann <fibsifan+github@gmail.com>
2019-10-26 03:13:49 +0200

    View WTFs in File using a Service mock

--- src/main/java/de/dotsource/wtf/intellij/WtfApplication.java
+++ src/main/java/de/dotsource/wtf/intellij/WtfApplication.java
@@ -4,33 +4,59 @@ import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.lang.Language;
 import com.intellij.lang.LanguageExtensionPoint;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.extensions.PluginId;
 import de.dotsource.wtf.intellij.editor.WtfExternalAnnotator;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 public class WtfApplication implements BaseComponent {
+    private static final Logger LOG = LoggerFactory.getLogger(WtfApplication.class);
     private IdeaPluginDescriptor plugin;
+    private ConcurrentMap<String, LanguageExtensionPoint> annotatorsByLanguage;
 
     @Override
     public void initComponent() {
+        annotatorsByLanguage = new ConcurrentHashMap<>();
         plugin = PluginManager.getPlugin(PluginId.getId("de.dotsource.wtf.intellij"));
         Language.getRegisteredLanguages().forEach(this::registerExternalAnnotatorFor);
+
+        deregisterRemovedLanguages();
     }
 
     private void registerExternalAnnotatorFor(final Language language) {
+
         LanguageExtensionPoint<WtfExternalAnnotator> extensionPoint = new LanguageExtensionPoint<>();
         extensionPoint.language = language.getID();
         extensionPoint.implementationClass = WtfExternalAnnotator.class.getName();
         extensionPoint.setPluginDescriptor(plugin);
         getExternalAnnotatorExtensionPoint().registerExtension(extensionPoint);
+
+        annotatorsByLanguage.put(language.getID(), extensionPoint);
     }
 
     @NotNull
     private ExtensionPoint<Object> getExternalAnnotatorExtensionPoint() {
         return Extensions.getRootArea().getExtensionPoint("com.intellij.externalAnnotator");
+    }
+
+    private void deregisterRemovedLanguages() {
+        Set<String> toRemove = annotatorsByLanguage.keySet()
+                .stream()
+                .filter(id -> Language.findLanguageByID(id) == null)
+                .collect(Collectors.toSet());
+
+        toRemove.forEach(l -> {
+            getExternalAnnotatorExtensionPoint().unregisterExtension(annotatorsByLanguage.get(l));
+            annotatorsByLanguage.remove(l);
+        });
     }
 }
--- src/main/java/de/dotsource/wtf/intellij/editor/WtfExternalAnnotator.java
+++ src/main/java/de/dotsource/wtf/intellij/editor/WtfExternalAnnotator.java
@@ -4,9 +4,13 @@ import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.ExternalAnnotator;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import de.dotsource.wtf.data.FeedbackEntry;
 import de.dotsource.wtf.data.FileMetricEntry;
 import de.dotsource.wtf.data.LineFeedbackEntry;
 import de.dotsource.wtf.service.FeedbackService;
@@ -15,10 +19,48 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class WtfExternalAnnotator extends ExternalAnnotator<FileMetricEntry, FileMetricEntry> {
     private static final Logger LOG = LoggerFactory.getLogger(WtfExternalAnnotator.class);
     private static final TextAttributesKey WTF = TextAttributesKey.createTextAttributesKey("WTF_WTF");
-    private FeedbackService feedbackService;
+    private static final Random random = new Random();
+    private Document document;
+    private FeedbackService feedbackService = new FeedbackService() {
+        @Override
+        public FileMetricEntry storeFeedback(FeedbackEntry entry) {
+            LOG.error("feedback stored.");
+            return new FileMetricEntry();
+        }
+
+        @Override
+        public FileMetricEntry getFeedbackForFile(String Path) {
+            LOG.error("feedback loaded.");
+            return generateFileMetricEntry(lines);
+        }
+    };
+
+    static {
+        LOG.warn("Class loaded.");
+    }
+
+    private int lines;
+
+    private static FileMetricEntry generateFileMetricEntry(int lines) {
+        FileMetricEntry fme = new FileMetricEntry();
+        for (int i = 0; i < lines; i++) {
+            fme.put(i, generateLineEntry(i+1));
+        }
+        return fme;
+    }
+
+    private static LineFeedbackEntry generateLineEntry(int linenumber) {
+        return new LineFeedbackEntry(random.nextDouble(), Double.valueOf(random.nextDouble()).toString());
+    }
+
+    public WtfExternalAnnotator() {
+        LOG.warn("Class instantiated.");
+    }
 
     @Nullable
     @Override
@@ -29,6 +71,8 @@ public class WtfExternalAnnotator extends ExternalAnnotator<FileMetricEntry, Fil
     @Nullable
     @Override
     public FileMetricEntry collectInformation(@NotNull PsiFile file) {
+        lines = PsiDocumentManager.getInstance(file.getProject()).getDocument(file).getLineCount();
+        document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
         return feedbackService.getFeedbackForFile(file.getVirtualFile().getPath());
     }
 
@@ -40,11 +84,15 @@ public class WtfExternalAnnotator extends ExternalAnnotator<FileMetricEntry, Fil
 
     @Override
     public void apply(@NotNull PsiFile file, FileMetricEntry annotationResult, @NotNull AnnotationHolder holder) {
-        annotationResult.entrySet().forEach(lineEntry -> addAnnotation(lineEntry.getKey(), lineEntry.getValue()));
+        annotationResult.entrySet().forEach(lineEntry -> addAnnotation(lineEntry.getKey(), lineEntry.getValue(), holder));
     }
 
-    private void addAnnotation(Integer key, LineFeedbackEntry value) {
-        Annotation annotation = new Annotation(key, key, HighlightSeverity.ERROR, "WTF", "WTF!");
+    private void addAnnotation(Integer key, LineFeedbackEntry value, AnnotationHolder holder) {
+        Annotation annotation = holder.createAnnotation(HighlightSeverity.ERROR, getTextRangeForLine(key+1), "WTF" + value.getRating(), "WTF!");
+    }
 
+    @NotNull
+    private TextRange getTextRangeForLine(Integer lineNumber) {
+        return new TextRange(document.getLineStartOffset(lineNumber), document.getLineEndOffset(lineNumber));
     }
 }
